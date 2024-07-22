d9eb46122c4fd5b631707ec782d32bc83310329c
ayoub yao <102902435+ayoubyao@users.noreply.github.com>
2024-07-22 13:44:58 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,6 @@
 const config = {
-  db: {
-    host: "zoo-express-1970.mysql.b.osc-fr1.scalingo-dbs.com",
-    user: "zoo_express_1970",
-    password: process.env.DATABASEPASSWORD,
-    database: "zoo_express_1970",
-    port:34612,
-    connectTimeout: 60000
-  },
+  db: process.env.DATABASE_URL,
   listPerPage: 10,
 };
+module.exports = config;
 module.exports = config; 
