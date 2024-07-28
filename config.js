63e9f2e9a34639696219bad22e60b01ea3ed9b3f
ayoubyao1210@gmail.com <ayoubyao1210@gmail.com>
2024-07-28 23:33:03 +0400

    ** change config

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 const config = {
   db: {
-    host: "mysql-179289-0.cloudclusters.net:10036",
+    host: "mysql-179289-0.cloudclusters.net",
     user: "alasquality",
+    port: 10036,
     password: process.env.DATABASEPASSWORD,
     database: "zoodb",
     connectTimeout: 60000
